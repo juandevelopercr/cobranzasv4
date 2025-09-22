5b7ef25a9a90a88bcc9147ce443d5a8bbd6c341e
@juandevelopercr <juandevcv@gmail.com>
2025-09-22 08:56:48 -0600

    Se adicion las traducciones del menu de casos

--- resources/views/livewire/casos/lafise-datatable.blade.php
+++ resources/views/livewire/casos/lafise-datatable.blade.php
@@ -56,7 +56,7 @@
             </div>
           </div>
 
-          @can("view-scotiabank-casos")
+          @can("view-lafise-casos")
             <div class="card-datatable table-responsive">
               <table class="table table-sm mb-0 border-top table-hover dataTable no-footer" id="casos-lafise-table" style="width: 100%;">
                 <thead>
