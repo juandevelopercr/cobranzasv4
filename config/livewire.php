8c8c4241cc16fd2a5bf4ddc0a717b2f8d1adc569
Juan Alberto Cáceres Vega <caceresvega@gmail.com>
2025-10-22 21:28:36 -0600

    Se actualiza configuracion de livewire para archivos grandes

--- config/livewire.php
+++ config/livewire.php
@@ -65,7 +65,8 @@ return [
 
   'temporary_file_upload' => [
     'disk' => 'local',        // Example: 'local', 's3'              | Default: 'default'
-    'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+    //'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+    'rules' => ['required', 'file', 'max:102400'], // 100 MB
     'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
     //'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
     'middleware' => ['web', 'auth:sanctum'], // Asegúrate que web va primero
