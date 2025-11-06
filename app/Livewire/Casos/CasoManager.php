00e257bdf4409079c0bab6b35eb1c618ff235aed
Juan Alberto Cáceres Vega <caceresvega@gmail.com>
2025-11-06 08:31:35 -0600

    update

--- app/Livewire/Casos/CasoManager.php
+++ app/Livewire/Casos/CasoManager.php
@@ -1321,7 +1321,7 @@ class CasoManager extends BaseComponent
   public function setEstadoProcesal(&$caso, &$errores, $fila)
   {
       if (!empty($caso->product_id) && $caso->product_id > 0 && !empty($caso->aestado_proceso_general_id)) {
-          $estado = CasoEstado::join('casos_estados_bancosX', function ($join) use ($caso) {
+          $estado = CasoEstado::join('casos_estados_bancos', function ($join) use ($caso) {
                   $join->on('casos_estados_bancos.estado_id', '=', 'casos_estados.id')
                       ->where('casos_estados_bancos.bank_id', $caso->banco_id);
               })
